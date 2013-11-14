dda2c393df47f9d2ba0f19924f08440b1301e642
harupong <harupong@gmail.com>
2013-11-14 11:33:00 +0900

    Remove whitespaces

--- lib/ripdiru.rb
+++ lib/ripdiru.rb
@@ -12,12 +12,12 @@ require 'fileutils'
 
 module Ripdiru
   class DownloadTask
-  
+
     TMPDIR = ENV['TMPDIR'] || '/tmp'
     SCHEDULE_URL = "http://www2.nhk.or.jp/hensei/api/noa.cgi?c=3&wide=1&mode=json"
-  
+
     attr_accessor :station, :cache, :buffer, :outdir, :bitrate
-  
+
     def initialize(station = nil, duration = 1800, *args)
       unless station
         abort "Usage: ripdiru [station-id]"
@@ -30,7 +30,7 @@ module Ripdiru
       @outdir = ENV['RIPDIRU_OUTDIR'] || "#{ENV['HOME']}/Music/Radiru"
       @bitrate = ENV['RIPDIRU_BITRATE'] || '48k'
     end
-  
+
     def channel
       case station
         when "NHK1"
@@ -46,21 +46,21 @@ module Ripdiru
           puts "invalid channel"
       end
     end
-  
+
     def val(element, path)
       element.get_text(path)
     end
-  
+
     def parse_time(str)
       DateTime.strptime("#{str}+0900", "%Y-%m-%d %H:%M:%S%Z").to_time
     end
-  
+
     def now_playing(station)
       now = Time.now
 
       f = open(SCHEDULE_URL)
       xml = REXML::Document.new(f)
-  
+
       REXML::XPath.each(xml, "//item") do |item|
         if val(item, "ch") == @mms_ch && val(item, "index") == '0'
           from, to = parse_time(val(item, "starttime")), parse_time(val(item, "endtime"))
@@ -81,20 +81,20 @@ module Ripdiru
     def mms_url
       f = open(@aspx)
       doc = REXML::Document.new(f)
-    
+
       mms_url =  REXML::XPath.first(doc, "//ENTRY/REF").attribute("HREF").to_s
       mms_url.sub!("mms://", "mmsh://")
     end
 
     def run
       program = now_playing(station)
-  
+
       duration = program.recording_duration + buffer
-  
+
       tempfile = "#{TMPDIR}/#{program.id}.mp3"
       puts "Streaming #{program.title} ~ #{program.to.strftime("%H:%M")} (#{duration}s)"
       puts "Ripping audio file to #{tempfile}"
-  
+
       command = %W(
         ffmpeg -y -i #{mms_url} -vn
         -loglevel error
@@ -109,20 +109,20 @@ module Ripdiru
         -t #{duration}
         #{tempfile}
       )
-  
+
       system command.join(" ")
-  
+
       FileUtils.mkpath(outdir)
       File.rename tempfile, "#{outdir}/#{program.id}.mp3"
-  
+
     end
-  
+
     def abort(msg)
       puts msg
       exit 1
     end
   end
-  
+
   class Program
     attr_accessor :id, :station, :title, :from, :to, :duration, :info
     def initialize(args = {})
@@ -130,24 +130,24 @@ module Ripdiru
         send "#{k}=", v
       end
     end
-  
+
     def effective_date
       time = from.hour < 5 ? from - 24 * 60 * 60 : from
       Date.new(time.year, time.month, time.day)
     end
-  
+
     def recording_duration
       (to - Time.now).to_i
     end
   end
-  
+
   class CacheDir
     attr_accessor :dir
     def initialize(dir)
       @dir = dir
       @paths = {}
     end
-  
+
     def [](name)
       @paths[name] ||= Pathname.new(File.join(@dir, name))
     end
